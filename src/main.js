4d0cd97579a1437fc8a065a904609c58937e063b
Dan <dan784@gmail.com>
2020-04-15 17:04:22 +0300

    first

---
+++ src/main.js
@@ -0,0 +1,48 @@
+import * as pot from './pot.js';
+import './style.css';
+
+function setMinSegmentLength(e) {
+    pot.setMinSegmentLength(e.target.value);
+}
+
+function setRandSegmentLength(e) {
+    pot.setRandSegmentLength(e.target.value);
+}
+
+function setColorChangeRate(e) {
+    pot.setColorChangeRate(e.target.value);
+}
+
+function setFade(e) {
+    pot.setFade(e.target.value);
+}
+
+function setMinJointRadius(e) {
+    pot.setMinJointRadius(e.target.value);
+}
+
+function setRandJointRadius(e) {
+    pot.setRandJointRadius(e.target.value);
+}
+
+function setMinJointAngle(e) {
+    pot.setMinJointAngle(e.target.value);
+}
+
+function setRandJointAngle(e) {
+    pot.setRandJointAngle(e.target.value);
+}
+
+//input functions
+function init() {
+    document.getElementById("minSegmentLength").addEventListener('input', setMinSegmentLength);
+    document.getElementById("randSegmentLength").addEventListener('input', setRandSegmentLength);
+    document.getElementById("colorChangeRate").addEventListener('input', setColorChangeRate);
+    document.getElementById("fade").addEventListener('input', setFade);
+    document.getElementById("minJointRadius").addEventListener('input', setMinJointRadius);
+    document.getElementById("randJointRadius").addEventListener('input', setRandJointRadius);
+    document.getElementById("minJointAngle").addEventListener('input', setMinJointAngle);
+    document.getElementById("randJointAngle").addEventListener('input', setRandJointAngle);
+}
+
+window.addEventListener('load', init);
---
+++ src/pot.js
@@ -0,0 +1,131 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext("2d");
+
+let width = canvas.width;
+let height = canvas.height;
+
+ctx.fillStyle = "black"
+ctx.strokeStyle = "#29ff7e"
+ctx.lineWidth = 10;
+
+ctx.fillRect(0, 0, width, height);
+
+let angle = (Math.floor(Math.random() * 9) - 4) * 0.7853981634;
+
+let minSegementLength = 30;
+let randSegmentLength = 0;
+let colorChangeRate = 50;
+let fade = 0.07;
+let minJointRadius = 20;
+let randJointRadius = 0;
+let minJointAngle = 0.7853981634;
+let randJointAngle = 0;
+
+//start position
+let startX = width / 4
+let startY = height / 4;
+
+let x = startX;
+let y = startY;
+
+//"cam"
+var camPosX = startX;
+var targetCamPosX = 300;
+
+var camPosY = startY;
+var targetCamPosY = 300;
+
+function drawNextSegment() {
+
+
+    ctx.shadowBlur = 20;
+    ctx.shadowColor = "transparent";
+
+    //fade last
+    ctx.fillStyle = "rgba(0,0,0," + fade + ")";
+    ctx.fillRect(0, 0, 600, 600);
+
+
+    let dist = +minSegementLength + Math.random() * randSegmentLength;
+
+    let hue = performance.now() / colorChangeRate % 360
+
+    ctx.strokeStyle = "white";
+
+    ctx.shadowBlur = 50;
+    ctx.shadowColor = `hsl(${hue},100%,70%`;
+
+    //line
+    let nextX = x + Math.cos(angle) * dist;
+    let nextY = y + Math.sin(angle) * dist;
+
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(nextX, nextY);
+    ctx.stroke();
+
+    // let nextAngle = (angle + Math.ceil(Math.random() * 3) * 0.7853981634) % (Math.PI * 2);
+    let nextAngle = angle + (Math.random() * randJointAngle) + minJointAngle;
+
+    let cornerRad = Math.random() * randJointRadius + minJointRadius;
+
+    let cornerCenterX = nextX + (Math.cos(angle + Math.PI / 2)) * cornerRad;
+    let cornerCenterY = nextY + (Math.sin(angle + Math.PI / 2)) * cornerRad;
+
+    ctx.beginPath();
+    ctx.arc(cornerCenterX, cornerCenterY, cornerRad, angle - Math.PI / 2, nextAngle - Math.PI / 2, false);
+    ctx.stroke();
+
+    //set to arc endpoint
+    x = cornerCenterX + Math.cos(nextAngle - Math.PI / 2) * cornerRad;
+    y = cornerCenterY + Math.sin(nextAngle - Math.PI / 2) * cornerRad;
+
+    angle = nextAngle;
+
+    if (x > width || x < 0 || y > height || y < 0) {
+        x = startX;
+        y = startY;
+        angle = (Math.floor(Math.random() * 9) - 4) * 0.7853981634;
+    }
+}
+
+function render(d) {
+
+    drawNextSegment();
+
+    requestAnimationFrame(render)
+}
+
+export function setMinSegmentLength(val) {
+    minSegementLength = +val;
+}
+
+export function setRandSegmentLength(val) {
+    randSegmentLength = +val;
+}
+
+export function setColorChangeRate(val) {
+    colorChangeRate = +val;
+}
+
+export function setFade(val) {
+    fade = +val / 255;
+}
+
+export function setMinJointRadius(val) {
+    minJointRadius = +val;
+}
+
+export function setRandJointRadius(val) {
+    randJointRadius = +val;
+}
+
+export function setMinJointAngle(val) {
+    minJointAngle = +val;
+}
+
+export function setRandJointAngle(val) {
+    randJointAngle = +val;
+}
+
+render();
