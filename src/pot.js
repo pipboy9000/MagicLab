a557db1b7c645c832a443ba13a8a38db56ab3eb4
Dan <dan784@gmail.com>
2020-04-16 01:56:50 +0300

    canvas will now center on shape

--- src/pot.js
+++ src/pot.js
@@ -3,10 +3,13 @@ let ctx = canvas.getContext("2d");
 
 let width = canvas.width;
 let height = canvas.height;
+let halfWidth = width / 2;
+let halfHeight = height / 2;
 
 ctx.fillStyle = "black"
 ctx.strokeStyle = "#29ff7e"
 ctx.lineWidth = 10;
+ctx.lineCap = 'round';
 
 ctx.fillRect(0, 0, width, height);
 
@@ -22,11 +25,14 @@ let minJointAngle = 0.7853981634;
 let randJointAngle = 0;
 
 //start position
-let startX = width / 4
-let startY = height / 4;
+let startX = width / 2
+let startY = height / 2;
 
 let x = startX;
 let y = startY;
+
+//points to calculate center
+let points = [];
 
 //"cam"
 var camPosX = startX;
@@ -35,18 +41,66 @@ var targetCamPosX = 300;
 var camPosY = startY;
 var targetCamPosY = 300;
 
+function drawPoint(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, 15, 0, Math.PI * 2);
+    ctx.stroke();
+}
+
+function setCamPos(x, y) {
+    targetCamPosX = x;
+    targetCamPosY = y;
+}
+
+function centerCanvas() {
+    let dx = (targetCamPosX - camPosX) / 5;
+    let dy = (targetCamPosY - camPosY) / 5;
+
+    camPosX += dx;
+    camPosY += dy;
+
+    ctx.setTransform(
+        1,
+        0,
+        0,
+        1,
+        -camPosX + halfWidth,
+        -camPosY + halfHeight
+    );
+}
+
+function avgPoints() {
+    let avg = points.reduce((acc, p, i) => {
+        acc.x += p.x;
+        acc.y += p.y;
+        return acc;
+    }, { x: 0, y: 0 });
+
+    avg.x /= points.length;
+    avg.y /= points.length;
+
+    //lower to resolution of 10 to avoid 
+    // avg.x = Math.round(avg.x / 10) * 10;
+    // avg.y = Math.round(avg.y / 10) * 10;
+
+    console.log(avg);
+
+    return avg;
+}
+
+function fadeCanvas() {
+    ctx.fillStyle = "rgba(0,0,0," + fade + ")";
+    ctx.fillRect(-halfWidth + camPosX, -halfHeight + camPosY, width, height);
+}
+
 function drawNextSegment() {
 
-
-    ctx.shadowBlur = 20;
+    //turn off glow before drawing fade layer
     ctx.shadowColor = "transparent";
 
-    //fade last
-    ctx.fillStyle = "rgba(0,0,0," + fade + ")";
-    ctx.fillRect(0, 0, 600, 600);
+    fadeCanvas();
 
-
-    let dist = +minSegementLength + Math.random() * randSegmentLength;
+    let dist = minSegementLength + Math.random() * randSegmentLength;
 
     let hue = performance.now() / colorChangeRate % 360
 
@@ -87,6 +141,17 @@ function drawNextSegment() {
         y = startY;
         angle = (Math.floor(Math.random() * 9) - 4) * 0.7853981634;
     }
+
+    //move camera
+    points.push({ x: nextX, y: nextY });
+    if (points.length > 30) {
+        points.shift();
+    }
+
+    let avg = avgPoints();
+    setCamPos(avg.x, avg.y);
+    drawPoint(avg.x, avg.y)
+    centerCanvas();
 }
 
 function render(d) {
@@ -94,8 +159,12 @@ function render(d) {
     drawNextSegment();
 
     requestAnimationFrame(render)
+
 }
 
+render();
+
+//setters
 export function setMinSegmentLength(val) {
     minSegementLength = +val;
 }
@@ -127,5 +196,3 @@ export function setMinJointAngle(val) {
 export function setRandJointAngle(val) {
     randJointAngle = +val;
 }
-
-render();
